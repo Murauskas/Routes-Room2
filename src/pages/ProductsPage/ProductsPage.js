184cabe810d42d929d18ed4cb77f86014820612c
Murauskas <rimantas.murauskas20@gmail.com>
2023-03-04 14:17:48 +0200

    ReactRouter1PaskaitaPatobulinta

--- src/pages/ProductsPage/ProductsPage.js
+++ src/pages/ProductsPage/ProductsPage.js
@@ -1,14 +1,16 @@
 import Product from "./components/Product/Product";
 import products from "../../components/productsData/productsData.json";
+import "../ProductsPage/ProductsPage.css"
 
 const data = JSON.parse(JSON.stringify(products));
 
 const ProductsPage = () => {
   return (
-    <div>
+    <div className="product-data">
       {data.map((product) => {
         return (
           <Product
+            className="product"
             id={product.id}
             label={product.label}
             key={product.id}
